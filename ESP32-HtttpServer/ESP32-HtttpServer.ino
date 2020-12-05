1fafaefe872a636d3f06508be02cb3e6daef72ef
Benjamin Habegger <b@habegger.fr>
2020-12-05 20:52:22 +0100

    Started sending camera data

--- ESP32-HtttpServer/ESP32-HtttpServer.ino
+++ ESP32-HtttpServer/ESP32-HtttpServer.ino
@@ -3,6 +3,9 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <Wire.h>
+#include "esp_camera.h"
+#include "camera_pins.h"
+#include "HttpServerIndex.h"
 #include "WifiCredentials.h"
 
 #define I2C_SDA 15
@@ -11,17 +14,56 @@
 WebServer server(80);
 
 const int led = 33;
-const char* indexHtml = "<html>\n  <head>\n<script\n  src=\"https://code.jquery.com/jquery-3.5.1.min.js\"\n  integrity=\"sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=\"\n  crossorigin=\"anonymous\">\n</script>\n<link\n  rel=\"stylesheet\" \n  href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\"\n  integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\"\n  crossorigin=\"anonymous\">\n</link>\n<script\n  src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\"\n  integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\"\n  crossorigin=\"anonymous\">\n</script>\n<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>\n<script>\nconst key_enter = 13;\nconst key_up    = 38;\nconst key_down  = 40;\nconst key_right = 39;\nconst key_left  = 37;\nconst key_esc   = 27;\nconst key_space = 32;\nconst key_ctrl  = 17;\nconst key_alt   = 18;\nconst key_shift = 16;\n\nfunction updateConsole(data,status,jqXHR) {\n  $(\"#console\").html(data);\n}\n\nvar commands = {\n  stop: function() {\n    console.log(\"Stop\");\n    $.get(\"/stop\",null,updateConsole);\n  },\n  forward: function() {\n    console.log(\"Forward\");\n    $.get(\"/forward\",null,updateConsole)\n  },\n  backward: function() {\n    console.log(\"Backward\");\n    $.get(\"/backward\",null,updateConsole)\n  },\n  left: function() {\n    console.log(\"Left\");\n    $.get(\"/left\",null,updateConsole)\n  },\n  right: function() {\n    console.log(\"Right\");\n    $.get(\"/right\",null,updateConsole)\n  }\n}\n\n$(function() {\n  $(\"body\").keydown(function(evt) {\n    switch(evt.which) {\n       case key_up: \n         commands.forward();\n         break;\n       case key_down:\n         commands.backward();\n         break;\n      case key_right:\n         commands.right();\n         break;\n      case key_left:\n         commands.left();\n         break;\n      case key_space:\n         commands.stop();\n         break;\n    }\n  });\n\n  $(\"#stop\").click(commands.stop);\n  $(\"#forward\").click(commands.forward);\n  $(\"#backward\").click(commands.backward);\n  $(\"#left\").click(commands.left);\n  $(\"#right\").click(commands.right);\n});\n\n</script>\n</head>\n<body>\n  <div class=\"container-fluid\">\n    <div id=\"cam\" class=\"alert alert-secondary d-flex justify-content-center\" role=\"alert\">\n    </div>\n  </div>\n<div class=\"container\">\n  <div class=\"row p-2\">\n    <div class=\"col-sm d-flex justify-content-center\">\n    </div>\n    <div class=\"col-sm d-flex justify-content-center\">\n      <button id=\"forward\" type=\"button\" class=\"btn btn-dark\">Forward</button>\n    </div>\n    <div class=\"col-sm d-flex justify-content-center\">\n    </div>\n  </div>\n  <div class=\"row p-2\">\n    <div class=\"col-sm d-flex justify-content-center\">\n      <button id=\"left\" type=\"button\" class=\"btn btn-dark\">Left</button>\n    </div>\n    <div class=\"col-sm d-flex justify-content-center\">\n      <button id=\"stop\" type=\"button\" class=\"btn btn-dark\">Stop</button>\n    </div>\n    <div class=\"col-sm d-flex justify-content-center\">\n      <button id=\"right\" type=\"button\" class=\"btn btn-dark\">Right</button>\n    </div>\n  </div>\n    <div class=\"row p-2\">\n    <div class=\"col-sm d-flex justify-content-center\">\n    </div>\n    <div class=\"col-sm d-flex justify-content-center\">\n      <button id=\"backward\" type=\"button\" class=\"btn btn-dark\">Backward</button>\n    </div>\n    <div class=\"col-sm d-flex justify-content-center\">\n    </div>\n  </div>\n  </div>\n  <div class=\"container-fluid\">\n    <div id=\"console\" class=\"alert alert-secondary d-flex justify-content-center\" role=\"alert\">\n          Welcome\n    </div>\n  </div>\n</body>\n</html>";
+const char* indexHtml = INDEX_HTML;
 
 void setup(void) {
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
 
+  setupCamera();
   setupI2C();
   setupWifi();
   setupMDNS();
   setupHttpEndpoints();
+}
+
+void setupCamera() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  config.frame_size = FRAMESIZE_VGA;
+  esp_camera_init(&config);
+
+
 }
 
 void setupI2C() {
@@ -97,6 +139,19 @@ void setupHttpEndpoints() {
     server.send(200, "text/plain", "Activating automatic mode");
   });
 
+  server.on("/capture", []() {
+    camera_fb_t * fb = esp_camera_fb_get();
+    if(!fb) {
+        server.send(500, "text/pain", "Camera failed to capture image");
+    } else {
+        server.sendHeader("Content-Disposition", "inline; filename=capture.jpg");
+        server.sendHeader("Access-Control-Allow-Origin", "*");
+        server.setContentLength(fb->len);
+        server.send(200, "image/jpeg", "");
+        server.client().write((const char *)fb->buf, fb->len);
+        esp_camera_fb_return(fb);
+    }
+  });
   server.onNotFound(handleNotFound);
 
   server.begin();
